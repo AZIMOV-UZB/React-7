e1cc2335bc5c62f9deeb9445c874428c38471bdf
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-13 13:06:29 -0700

    react

--- src/pages/shopping/Shopping.jsx
+++ src/pages/shopping/Shopping.jsx
@@ -2,14 +2,47 @@ import React from 'react'
 
 const Shopping = () => {
   return (
-    <div className=''>
-    <div className=' container flex-col-reverse	 '>
-<input required className='block	 outline-none	 mx-3 mt-6 text-xl font-normal'  type="text"  placeholder='maxsulot nom '/>
-<input required className='block	 outline-none	 mx-3 mt-6 text-xl font-normal' type="number" placeholder='maxsulotga bergan baxoyingiz ' />
-<input required className='block	 outline-none	 mx-3 mt-6 text-xl font-normal' type="text" placeholder="to'lv tur"/>  
+    <div className=' container'>
+    <div className=' justify-center m-auto'>
+      <div className='flex  my-3'>
+      <label htmlFor="">NAME :</label>
+<input required className='items-center  -mt-	outline-none	 mx-3  text-xl font-light	'  type="text"  placeholder='Name '/>
+</div>
+      <div className='flex my-3'>
+      <label htmlFor="">Lastname :</label>
+<input required className='	 outline-none -mt-	 mx-3  text-xl font-light	'  type="text"  placeholder='Lastname'/>
+      </div>
 
+      <div className='flex my-3'>
+      <label htmlFor="">Email</label>
+<input required className='	 outline-none -mt-	 mx-3  text-xl font-light	'  type="text"  placeholder='********@gmail.com '/>
+      </div>
 
-<button>Submit</button>
+      <div className='flex my-3'>
+      <label htmlFor="">Phone nomber</label>
+<input required className='	 outline-none -mt-	 mx-3  text-xl font-light	'  type="number"  placeholder='998-**-***-**'/>
+      </div>
+
+      <div className='flex my-3'>
+      <label htmlFor="">Maxsulot nomi</label>
+<input required className='	 outline-none -mt-	 mx-3  text-xl font-light	'  type="text"  placeholder='Maxsulot nom '/>
+      </div>
+
+      <div className='flex my-3'>
+      <label htmlFor="">Maxsulotn baxolang </label>
+<input required className='	 outline-none -mt-	 mx-3  text-xl font-light	' type="number" placeholder='Maxsulotga bergan baxoyingiz ' />
+      </div>
+
+      <div className=' my-3'>
+      <label className='block	' htmlFor="">To'lov ko'rinish</label>
+      <label htmlFor="">Naqt</label>
+      <input className='mx-2 ' type="radio" name="rb" id="" />
+      <label htmlFor="">Card</label>
+      <input className='mx-2 ' type="radio" name="rb" id="" />
+
+      </div>
+
+<button className='px-11 py-2 my-6 rounded-xl	 text-xl bg-lime-400'>Submit</button>
     </div>
     </div>
   )
